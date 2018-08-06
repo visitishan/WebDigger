f6f5eb61524623d336c09d2259ef2d11bb3f727f
Ishan Jain <visitishan@gmail.com>
2018-08-07 00:55:49 +0530

    Functions created for similar tasks
    Function created for getting movie links and getting google sear results.

--- mov1.py
+++ mov1.py
@@ -2,6 +2,17 @@
 from bs4 import *
 import requests
 
+# List to stores Links from google search page
+sites = []
+movies = []
+googleallpages = [] # List to store next 9 pages of google search
+
+#File valid extensions (file links should end with these extensions)
+ext = ('mkv','mov','avi','mp4','mpg','wmv')
+
+# Browser Headers
+headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+
 #Name of file to be searched - Search Term
 name = input("Enter a movie name : ")
 
@@ -11,33 +22,35 @@ urlPref = "https://www.google.com/search?q="
 # URL Suffix
 urlSuff = "%20%2B(mkv%7Cmp4%7Cavi%7Cmov%7Cmpg%7Cwmv)%20-inurl%3A(jsp%7Cpl%7Cphp%7Chtml%7Caspx%7Chtm%7Ccf%7Cshtml)%20intitle%3Aindex.of%20-inurl%3A(listen77%7Cmp3raid%7Cmp3toss%7Cmp3drug%7Cindex_of%7Cwallywashis)"
 
-# Browser Headers
-headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
-
 # complete URL of first search page
-url = urlPref + name + urlSuff
+url1 = urlPref + name + urlSuff
 
-# Get URL page data
-page = requests.get(url, headers=headers)
+soup = ' '
+#Function to find and append the websites on a google search page into sites[] list
+def getResLinks(url):
+	# Get URL page data
+	page = requests.get(url, headers=headers)
+	# Get page data parsed as HTML
+	global soup
+	soup = BeautifulSoup(page.text, "html.parser")
+	#Scraping google searchpage links
+	links = soup.findAll("cite")
+	print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~ WEB FOLDER LINKS ~~~~~~~~~~~~~~~~~~~~~~~~")
+	for getL in links:
+		hrefs = getL.get_text()
+		sites.append(hrefs)
+		print(hrefs)
 
-# Get page data parsed as HTML
-soup = BeautifulSoup(page.text, "html.parser")
-
-# List to stores Links from google search page
-sites = []
-
-#Scraping google searchpage links
-links = soup.findAll("cite")
-for getL in links:
-    hrefs = getL.get_text()
-    sites.append(hrefs)
-    print(hrefs)
-
-movies = []
-
-#File valid extensions (file links should end with these extensions)
-ext = ('mkv','mov','avi','mp4','mpg','wmv')
-
+#function to find next 9 search pages of google and store them in googleallpages[] list
+def googlekpages():
+	flclasslinks = []
+	nextpage = soup.find_all("a",class_="fl")
+	for pages in nextpage:
+		pg = (pages['href'])
+		pg = 'http://google.com' + pg
+		flclasslinks.append(pg)
+	global googleallpages
+	googleallpages = (flclasslinks[-9:])
 
 #Function to check whether the Search term is present in final file link. It'll return 1 if matches else skip the link.
 def match(filekalink, searchterm):
@@ -51,29 +64,39 @@ def match(filekalink, searchterm):
 		return 1
 
 
-for site in sites:
-	if ((site.startswith("https://")) or (site.startswith("http://"))):
-		fullsitelink = site
-	else :
-		fullsitelink = "http://" + site
-	try :
-		innersite = requests.get(fullsitelink, headers=headers)
-		innersoup = BeautifulSoup(innersite.text, "html.parser")
-		alllinks = innersoup.findAll("a")
-		for movielink in alllinks:
-			movie = (movielink['href'])
-			milgaya = fullsitelink + movie
-			#if milgaya.endswith(('mkv','mov','avi','mp4','mpg','wmv')) :
-			if milgaya.endswith(ext) :
-				if match(milgaya, name) == 1:
-					print(milgaya)
-	except :
-		continue
+def getMovielink(websites):
+	print("~~~~~~~~~~~~~~~~~~~~~~~~ MOVIE LINKS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	for site in websites:
+		if ((site.startswith("https://")) or (site.startswith("http://"))):
+			fullsitelink = site
+		else :
+			fullsitelink = "http://" + site
+		try :
+			innersite = requests.get(fullsitelink, headers=headers)
+			innersoup = BeautifulSoup(innersite.text, "html.parser")
+			alllinks = innersoup.findAll("a")
+			for movielink in alllinks:
+				movie = (movielink['href'])
+				milgaya = fullsitelink + movie
+				#if milgaya.endswith(('mkv','mov','avi','mp4','mpg','wmv')) : ----depricated
+				if milgaya.endswith(ext) :
+					if match(milgaya, name) == 1:
+						print(milgaya)
+		except :
+			continue
 
 
+getResLinks(url1)
+getMovielink(sites)
 
-
-
+ask = input("Want more links? (Y/N)")
+if ask == 'Y':
+	googlekpages()
+	print(googleallpages)
+	for page in googleallpages:
+		getResLinks(page)
+	for onesite in sites:
+		getMovielink(sites)
 
 
 
