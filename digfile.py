80061fde03db02623b836d1b4fa4c0697bcf43c0
Mayur Rawte <m.r.rawte7@gmail.com>
2018-09-13 02:47:18 +0530

    Fake-useragent added for random user agent (#3)

--- digfile.py
+++ digfile.py
@@ -1,29 +1,15 @@
 # library imports
-from bs4 import *
+from bs4 import BeautifulSoup
 import requests
-import random
+from fake_useragent import UserAgent
+ua = UserAgent()
 
 # List to store Links from google search page
 sites = []
 movies = [] #List to store link of movies
 googleallpages = [] # List to store next 9 pages of google search
 
-# List containing different browser user agents.
-headerlist = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
-		   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991",
-           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 OPR/42.0.2393.94",
-           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36 OPR/47.0.2631.39",
-           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-           "Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36",
-           "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
-           "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
-           "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
-           "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0",
-           "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"]
-
-#Selecting a random user agent from the headerlist and saving it to header. This step is to prevent getting blocked from google search.
-user_agent = random.choice(headerlist)
-headers = {'User-Agent': user_agent}
+headers = {'User-Agent': ua.random}
 
 #Initializing variables with blank
 soup = ' '
